0f460587e203bdebe686786a130c78eb428eaef2
Kyle Spraggins <spraggkj@mail.uc.edu>
2021-07-05 17:36:17 -0400

    added core usage

--- Damage Calculation/Damage.cpp
+++ Damage Calculation/Damage.cpp
@@ -4,7 +4,14 @@
 
 int Damage::Calculation(int attackPower, int targetDefense)
 {
+	Core core = Core();
+	bool crit = core.CheckCriticalHit();
+
 	int damage = attackPower - targetDefense;
+	if (crit) {
+		damage = damage * 2;
+	}
+	
 	std::cout << "Damage Dealt: " + damage << std::endl;
 	return damage;
 }
